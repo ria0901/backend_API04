fd5097efc17be94187f8e65a81bb086ad4ee671c
Clarista Citra Marcellina <claristacitram@gmail.com>
2022-05-26 12:13:53 +0700

    Update Product.js

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,25 +1,48 @@
-// import sequelize 
-const Sequelize = require("sequelize");
-// import connection 
-const db = require("../config/database.js");
- 
-// init DataTypes
-const { DataTypes } = Sequelize;
- 
-// Define schema
-const Product = db.define('products', {
-  // Define attributes
-  title: {
-    type: DataTypes.STRING
-  },
-  price: {
-    type: DataTypes.DOUBLE
-  }
-},{
-  // Freeze Table Name
-  freezeTableName: true
-});
- 
-// Export model Product
+const db = require("../config/db")
+const { DataTypes } = require("sequelize");
+const Category = require("./Category");
+
+const Product = db.define(
+    "product",
+    {
+        product_id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        category_id: {
+            type: DataTypes.SMALLINT,
+            allowNull: false,
+            references: {
+                model: Category,
+                key: "category_id"
+            },
+        },
+        name: {
+            type: DataTypes.STRING(50),
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+        },
+        price: {
+            type: DataTypes.BIGINT,
+            allowNull: false,
+        },
+        stock: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        image: {
+            type: DataTypes.STRING(255),
+            allowNull: false,
+        }
+    }
+)
+
 module.exports = Product;
-// export default Product;
